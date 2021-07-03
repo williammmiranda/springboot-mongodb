6d56215d6da6d85cd42bb2d0b00568c835d08ff9
William Miranda <wmendes.miranda@gmail.com>
2021-07-03 13:29:20 -0300

    Criação do Projeto

---
+++ src/test/java/com/williammiranda/workshop/mongodb/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williammiranda.workshop.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
