bb1a7c30bc3acb2a280cc95847a339004aea5f13
William Miranda <wmendes.miranda@gmail.com>
2021-07-08 08:51:54 -0300

    Consulta simples com o @Query

--- src/main/java/com/williammiranda/workshop/mongodb/gateway/database/repository/PostRepository.java
+++ src/main/java/com/williammiranda/workshop/mongodb/gateway/database/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.williammiranda.workshop.mongodb.gateway.database.repository;
 
 import com.williammiranda.workshop.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,9 @@ import java.util.List;
 @Repository
 @EnableMongoRepositories
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
