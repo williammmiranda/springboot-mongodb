b86704b084b95546667c118ac442f211572475aa
William Miranda <wmendes.miranda@gmail.com>
2021-07-08 09:21:13 -0300

    Pesquisa com vários critérios

--- src/main/java/com/williammiranda/workshop/mongodb/resources/PostResource.java
+++ src/main/java/com/williammiranda/workshop/mongodb/resources/PostResource.java
@@ -6,6 +6,7 @@ import com.williammiranda.workshop.mongodb.services.PostService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,6 +29,17 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+                                                 @RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                                 @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.conversionDate(minDate, new Date(0L));
+        Date max = URL.conversionDate(maxDate, new Date());
+        List<Post> posts = postService.fullSeach(text, min, max);
         return ResponseEntity.ok(posts);
     }
 }
